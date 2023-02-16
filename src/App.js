814bac61cc4f8471268d3974c99517bdfcfe0861
William <williampv@gmail.com>
2023-02-16 15:42:43 -0500

    query product to API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import './App.css';
 import { ProductCard } from './components/ProductCard/ProductCard';
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
   const [products, setProducts] = useState([])
+
+  useEffect(()=>{
+    axios.get('https://fakestoreapi.com/products').then((result)=>{
+      setProducts(result.data)
+      console.log(products)
+    })
+  }, [])
+
   return (
     <div className="App">
       <h1>Products List</h1>
-      <ProductCard title="product test" price="12.50"/>
+      {products && products.map((product)=>{
+        return(
+          <ProductCard title={product.title} price={product.price}/>
+        )
+      })}
     </div>
   );
 }
