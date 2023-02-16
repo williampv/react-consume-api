d56bb5456eae2b889df4821328b6d03d70630e34
William <williampv@gmail.com>
2023-02-16 15:22:00 -0500

    ProductCard component created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { ProductCard } from './components/ProductCard/ProductCard';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [products, setProducts] = useState([])
   return (
     <div className="App">
       <h1>Products List</h1>
+      <ProductCard title="product test" price="12.50"/>
     </div>
   );
 }
